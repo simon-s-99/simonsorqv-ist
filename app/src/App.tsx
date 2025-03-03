d4a7c09dfa537b05694f2668f2a8dfc65a7771ea
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2025-03-03 17:34:30 +0100

    feat: add 'coming soon' layout & message

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,32 +1,18 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import NavBar from "./components/NavBar";
 
 export default function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <NavBar />
+
+      <main className="font-mono flex flex-col place-self-center mt-4">
+        <p>This website is under development, content coming soon.</p>
+        <p>In the meantime you can check out my:</p>
+        <ul className="flex flex-col items-center mt-1">
+          <li className="mt-2"><a href="https://github.com/simon-s-99">GitHub</a></li>
+          <li className="mt-2"><a href="https://www.linkedin.com/in/simon-sorqvist/">LinkedIn</a></li>
+        </ul>
+      </main>
     </>
   );
 }
---
+++ app/src/components/NavBar.tsx
@@ -0,0 +1,10 @@
+export default function NavBar() {
+  return (
+    <header className="font-mono font-bold flex place-self-center pt-12">
+      <nav className="flex flex-col place-self-center">
+        <h1>Simon Sörqvist</h1>
+        <p className="mx-auto">Developer</p>
+      </nav>
+    </header>
+  );
+}
