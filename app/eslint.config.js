ce8cb1cf0c5b784a0ba720cafd3e942c94c2c54e
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2025-03-03 15:21:12 +0100

    configure basic eslint with @stylistic/eslint

--- app/eslint.config.js
+++ app/eslint.config.js
@@ -1,28 +1,34 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
+import js from "@eslint/js"
+import globals from "globals"
+import reactHooks from "eslint-plugin-react-hooks"
+import reactRefresh from "eslint-plugin-react-refresh"
+import tseslint from "typescript-eslint"
+import stylisticJs from "@stylistic/eslint-plugin-js"
 
 export default tseslint.config(
-  { ignores: ['dist'] },
+  { ignores: ["dist"] },
   {
     extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      "@stylistic/js": stylisticJs,
     },
     rules: {
       ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
+      "@stylistic/js/indent": ["error", 2],
+      "@stylistic/js/semi": ["error", "always"],
+      "@stylistic/js/quotes": ["error", "double", { "allowTemplateLiterals": true }],
+      "@stylistic/js/jsx-quotes": ["error", "prefer-double"],
     },
   },
 )
