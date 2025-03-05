6858494e1a89e1620edd9138b564cb92ade8847d
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2025-03-05 20:28:15 +0100

    style: add on-hover effect to social links
    try to make it clearer that they are clickable buttons that do things

--- src/App.tsx
+++ src/App.tsx
@@ -9,8 +9,22 @@ export default function App() {
         <p>This website is under development, content coming soon.</p>
         <p>In the meantime you can check out my:</p>
         <ul className="flex flex-col items-center mt-1">
-          <li className="mt-2"><a href="https://github.com/simon-s-99">GitHub</a></li>
-          <li className="mt-2"><a href="https://www.linkedin.com/in/simon-sorqvist/">LinkedIn</a></li>
+          <li className="mt-4">
+            <a
+              href="https://github.com/simon-s-99"
+              className="px-1 py-0.5 border-2 border-gray-400 border-dotted hover:border-solid"
+            >
+              GitHub
+            </a>
+          </li>
+          <li className="mt-4">
+            <a
+              href="https://www.linkedin.com/in/simon-sorqvist/"
+              className="px-1 py-0.5 border-2 border-gray-400 border-dotted hover:border-solid"
+            >
+              LinkedIn
+            </a>
+          </li>
         </ul>
       </main>
     </>
