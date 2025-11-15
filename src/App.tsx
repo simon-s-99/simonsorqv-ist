f0e88196225bae684ecf28261ada078b479b3e68
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2025-11-15 18:50:05 +0100

    move 'under development' msg to banner

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,45 @@
 import CreatorNameSVG from "./components/CreatorNameSVG";
 import { ImGithub } from "react-icons/im";
 import { FaLinkedin } from "react-icons/fa";
+import Banner from "./components/Banner";
 
 export default function App() {
   return (
     /* minus-margin used here to offset slightly from middle of screen on desktop, remove later */
-    <div className="min-h-svh flex flex-col justify-center -mt-16">
+    <div className="min-h-svh flex flex-col">
       <header className="flex flex-col">
-        <nav className="flex flex-col gap-y-1 items-center text-[2.6rem]">
+        <Banner />
+        <nav className="flex flex-col gap-y-1 items-center text-[2.6rem] mt-32">
           <CreatorNameSVG width={280} height={160} />
           <h1>Developer</h1>
         </nav>
       </header>
-      <main className="flex flex-col items-center text-center mt-4">
-        <p>
-          This website is under{" "}
-          <span className="text-secondary-accent">development</span>, content
-          coming soon.
-        </p>
-        <p>In the meantime you can check out my:</p>
+      <main className="flex flex-col">
+        <section className="flex flex-col items-center text-center">
+          <p className="mt-2">Check out my:</p>
 
-        <ul className="flex flex-col items-center gap-y-1">
-          <li className="mt-5">
-            <a
-              href="https://github.simonsorqv.ist/"
-              className="flex flex-row gap-x-1 px-2 py-1.5 border-2 rounded-md bg-primary border-secondary-accent hover:text-secondary-accent"
-            >
-              <ImGithub className="text-2xl" /> GitHub
-            </a>
-          </li>
-          <li className="mt-4">
-            <a
-              href="https://linkedin.simonsorqv.ist/"
-              className="flex flex-row gap-x-1 px-2 py-1.5 border-2 rounded-md bg-primary border-secondary-accent hover:text-secondary-accent"
-            >
-              <FaLinkedin className="text-2xl" /> LinkedIn
-            </a>
-          </li>
-        </ul>
+          <ul className="flex flex-col items-center gap-y-1">
+            <li className="mt-5">
+              <a
+                href="https://github.simonsorqv.ist/"
+                className="flex flex-row gap-x-1 px-2 py-1.5 border-2 rounded-md bg-primary border-secondary-accent hover:text-secondary-accent"
+              >
+                <ImGithub className="text-2xl" /> GitHub
+              </a>
+            </li>
+            <li className="mt-4">
+              <a
+                href="https://linkedin.simonsorqv.ist/"
+                className="flex flex-row gap-x-1 px-2 py-1.5 border-2 rounded-md bg-primary border-secondary-accent hover:text-secondary-accent"
+              >
+                <FaLinkedin className="text-2xl" /> LinkedIn
+              </a>
+            </li>
+          </ul>
+        </section>
+        <section>{/* TODO: add section */}</section>
       </main>
+      <footer>{/* TODO: add footer */}</footer>
     </div>
   );
 }
---
+++ src/components/Banner.tsx
@@ -0,0 +1,10 @@
+export default function Banner() {
+  return (
+    <div className="bg-primary flex flex-col justify-center items-center h-8">
+      <p>
+        This website is under{" "}
+        <span className="text-secondary-accent">development</span>, content & layout may change at any time.
+      </p>
+    </div>
+  );
+}
