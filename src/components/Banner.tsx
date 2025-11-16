05371c98699b0fb03e0827bac31f47aa0f42c973
simon-s-99 <142667693+simon-s-99@users.noreply.github.com>
2025-11-16 16:05:03 +0100

    fix banner height

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,6 +1,6 @@
 export default function Banner() {
   return (
-    <div className="bg-primary flex flex-col justify-center items-center h-8">
+    <div className="bg-primary flex flex-col justify-center items-center min-h-8">
       <p>
         This website is under{" "}
         <span className="text-secondary-accent">development</span>, content & layout may change at any time.
